6f592a9991ce5279503689ff756a2981da45067a
cabadilla <54191657+cabadilla@users.noreply.github.com>
2021-10-20 00:58:27 -0600

    funciones administrador
    componentes de funciones de administrador

--- TeachableWeb/src/app/app.module.ts
+++ TeachableWeb/src/app/app.module.ts
@@ -10,11 +10,28 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 //componentes
 import { LoginComponent } from './components/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FunctionsComponent } from './components/admin/functions/functions.component';
+import { ManageCoursesComponent } from './components/admin/manage-courses/manage-courses.component';
+import { ManageUsersComponent } from './components/admin/manage-users/manage-users.component';
+import { InsertCourseComponent } from './components/admin/manage-courses/insert-course/insert-course.component';
+import { UpdateCourseComponent } from './components/admin/manage-courses/update-course/update-course.component';
+import { ManageStudentComponent } from './components/admin/manage-courses/manage-student/manage-student.component';
+import { InsertUserComponent } from './components/admin/manage-users/insert-user/insert-user.component';
+import { UpdateUserComponent } from './components/admin/manage-users/update-user/update-user.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    FunctionsComponent,
+    ManageCoursesComponent,
+    ManageUsersComponent,
+    InsertCourseComponent,
+    UpdateCourseComponent,
+    ManageStudentComponent,
+    InsertUserComponent,
+    UpdateUserComponent
   ],
   imports: [
     BrowserModule,
---
+++ TeachableWeb/src/app/components/Admin/manage-courses/insert-course/insert-course.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-insert-course',
+  templateUrl: './insert-course.component.html',
+  styleUrls: ['./insert-course.component.scss']
+})
+export class InsertCourseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
