b91a2e4ac35ae58b526104bca233b653b50c62bd
Allison <50599762+AllisonSFlores@users.noreply.github.com>
2021-10-20 16:10:59 -0600

    ruta de imports de Admin

--- TeachableWeb/src/app/app.module.ts
+++ TeachableWeb/src/app/app.module.ts
@@ -10,15 +10,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 //componentes
 import { LoginComponent } from './components/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
-
-import { FunctionsComponent } from './components/admin/functions/functions.component';
-import { ManageCoursesComponent } from './components/admin/manage-courses/manage-courses.component';
-import { ManageUsersComponent } from './components/admin/manage-users/manage-users.component';
-import { InsertCourseComponent } from './components/admin/manage-courses/insert-course/insert-course.component';
-import { UpdateCourseComponent } from './components/admin/manage-courses/update-course/update-course.component';
-import { ManageStudentComponent } from './components/admin/manage-courses/manage-student/manage-student.component';
-import { InsertUserComponent } from './components/admin/manage-users/insert-user/insert-user.component';
-import { UpdateUserComponent } from './components/admin/manage-users/update-user/update-user.component';
+import { FunctionsComponent } from './components/Admin/functions/functions.component';
+import { ManageCoursesComponent } from './components/Admin/manage-courses/manage-courses.component';
+import { ManageUsersComponent } from './components/Admin/manage-users/manage-users.component';
+import { InsertCourseComponent } from './components/Admin/manage-courses/insert-course/insert-course.component';
+import { UpdateCourseComponent } from './components/Admin/manage-courses/update-course/update-course.component';
+import { ManageStudentComponent } from './components/Admin/manage-courses/manage-student/manage-student.component';
+import { InsertUserComponent } from './components/Admin/manage-users/insert-user/insert-user.component';
+import { UpdateUserComponent } from './components/Admin/manage-users/update-user/update-user.component';
 import { ListCoursesComponent } from './components/list-courses/list-courses.component';
 
 
