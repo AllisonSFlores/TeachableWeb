723f9da1247e03ee76936e1591701e0001303413
cabadilla <54191657+cabadilla@users.noreply.github.com>
2021-10-21 00:33:05 -0600

    admin functions and styles

--- TeachableWeb/src/app/app-routing.module.ts
+++ TeachableWeb/src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FunctionsComponent } from './components/Admin/functions/functions.component';
+import { ManageCoursesComponent } from './components/Admin/manage-courses/manage-courses.component';
+import { ManageStudentComponent } from './components/Admin/manage-courses/manage-student/manage-student.component';
 import { ListCoursesComponent } from './components/list-courses/list-courses.component';
 import { LoginComponent } from './components/login/login.component';
 import { TeacherModule } from './teacherModule/teacher.module';
 const routes: Routes = [
   {path:'teacher/:id', loadChildren: () => import ('./teacherModule/teacher.module').then(x => x.TeacherModule)},
   {path: 'login', component:LoginComponent},
-  {path: 'listCourses',component:ListCoursesComponent}
+  {path: 'listCourses',component:ListCoursesComponent},
+  {path: 'admin',component:FunctionsComponent},
+  {path: 'manageCourses',component:ManageCoursesComponent},
+  {path: 'manageUsers',component:ManageStudentComponent}
+
 ];
 
 @NgModule({
